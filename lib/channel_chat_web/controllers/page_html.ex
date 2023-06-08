77db9e3d473cc309aa7bd73a7572d1f9ad3fa3bd
Henrique Rezende <henrique.64@hotmail.com>
2023-06-08 19:36:30 -0300

    feat: auth lib configs

--- lib/channel_chat_web/controllers/page_html.ex
+++ lib/channel_chat_web/controllers/page_html.ex
@@ -2,4 +2,8 @@ defmodule ChannelChatWeb.PageHTML do
   use ChannelChatWeb, :html
 
   embed_templates "page_html/*"
+
+  def person_name(person) do
+    person.givenName || person.name || "guest"
+  end
 end
--- lib/channel_chat_web/router.ex
+++ lib/channel_chat_web/router.ex
@@ -5,23 +5,18 @@ defmodule ChannelChatWeb.Router do
     plug :accepts, ["html"]
     plug :fetch_session
     plug :fetch_live_flash
-    plug :put_root_layout, html: {ChannelChatWeb.Layouts, :root}
+    plug :put_root_layout, {ChannelChatWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
   end
 
-  # pipeline :api do
-  #   plug :accepts, ["json"]
-  # end
+  pipeline :authOptional, do: plug(AuthPlugOptional)
 
   scope "/", ChannelChatWeb do
-    pipe_through :browser
+    pipe_through [:browser, :authOptional]
 
     get "/", PageController, :home
+    get "/login", AuthController, :login
+    get "/logout", AuthController, :logout
   end
-
-  # Other scopes may use custom stacks.
-  # scope "/api", ChannelChatWeb do
-  #   pipe_through :api
-  # end
 end
