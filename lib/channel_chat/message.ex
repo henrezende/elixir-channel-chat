5fcf6c75f8e3136fcba43b2e4a37584b6f650e04
Henrique Rezende <henrique.64@hotmail.com>
2023-06-08 14:27:07 -0300

    feat: message schema

---
+++ lib/channel_chat/message.ex
@@ -0,0 +1,26 @@
+defmodule ChannelChat.Message do
+  use Ecto.Schema
+  import Ecto.Changeset
+  import Ecto.Query
+
+  schema "messages" do
+    field :message, :string
+    field :name, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(message, attrs) do
+    message
+    |> cast(attrs, [:name, :message])
+    |> validate_required([:name, :message])
+  end
+
+  def get_messages(limit \\ 20) do
+    ChannelChat.Message
+    |> limit(^limit)
+    |> order_by(desc: :inserted_at)
+    |> ChannelChat.Repo.all()
+  end
+end
