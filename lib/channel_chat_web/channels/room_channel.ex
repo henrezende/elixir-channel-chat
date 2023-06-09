82651b27c3a4b3504fcdbfffd0fe79d7d85572a6
Henrique Rezende <henrique.64@hotmail.com>
2023-06-09 15:53:49 -0300

    feat: implement presence

--- lib/channel_chat_web/channels/room_channel.ex
+++ lib/channel_chat_web/channels/room_channel.ex
@@ -1,5 +1,6 @@
 defmodule ChannelChatWeb.RoomChannel do
   use ChannelChatWeb, :channel
+  alias ChannelChatWeb.Presence
 
   @impl true
   def join("room:lobby", payload, socket) do
@@ -22,23 +23,43 @@ defmodule ChannelChatWeb.RoomChannel do
   # broadcast to everyone in the current topic (room:lobby).
   @impl true
   def handle_in("shout", payload, socket) do
-    ChannelChat.Message.changeset(%ChannelChat.Message{}, payload) |> ChannelChat.Repo.insert
-    broadcast(socket, "shout", payload)
+    # Insert message in database
+    {:ok, msg} = ChannelChat.Message.changeset(%ChannelChat.Message{}, payload) |> ChannelChat.Repo.insert()
+
+    # Assigning name to socket assigns and tracking presence
+    socket
+      |> assign(:person_name, msg.name)
+      |> track_presence()
+      |> broadcast("shout", Map.put_new(payload, :id, msg.id))
+
     {:noreply, socket}
   end
+
 
   @impl true
   def handle_info(:after_join, socket) do
     ChannelChat.Message.get_messages()
     |> Enum.reverse() # revers to display the latest message at the bottom of the page
-    |> Enum.each(fn msg -> push(socket, "shout", %{
+    |> Enum.each(fn msg ->
+      push(socket, "shout", %{
         name: msg.name,
         message: msg.message,
-        inserted_at: msg.inserted_at,
-      }) end)
+        inserted_at: msg.inserted_at
+      })
+    end)
+
+    push(socket, "presence_state", Presence.list("room:lobby"))
+
     {:noreply, socket} # :noreply
   end
 
+  defp track_presence(%{assigns: %{person_name: person_name}} = socket) do
+    Presence.track(socket, person_name, %{
+      online_at: inspect(System.system_time(:second))
+    })
+
+    socket
+  end
   # Add authorization logic here as required.
   defp authorized?(_payload) do
     true
